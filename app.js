fcb40d2fb8005364d5da46570dd4bf98af075ec0
Fabricio Layedra <flayedra@espol.edu.ec>
2017-06-19 08:37:35 -0500

    JSON solved

---
+++ app.js
@@ -0,0 +1,56 @@
+var http_request = false;
+
+function makeRequest(url) {
+
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/plain');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = alertContents;
+    http_request.open('GET', url, true);
+    http_request.send(null);
+
+}
+
+function alertContents() {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            var json=http_request.responseText;
+            var titulos =JSON.parse(json);
+            let result =""
+            for(titulo of titulos){
+                let title=titulo.titulo;
+                result +="<li>"+title+"</li>";
+            }
+            document.getElementById("lista-canciones").innerHTML=result;
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('requerimiento');
+    link.onclick = function() {
+        makeRequest('datos.json');
+    }
+}
